d809a9a05b1bc3f7e3c40ce0db4f1c513f781a3f
Victor Sysout <victornoleto@sysout.com.br>
2023-09-27 09:42:25 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calculadora-ponto',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
